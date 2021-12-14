bb028dd78a5909ffc8435581fd4ee340b6f0a347
Hutao Doctrine <guy_zxc@hotmail.com>
2021-12-14 16:59:31 +0700

    change

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,7 +1,22 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+int printO(int x,int y)
+	{
+		if(x > 0 && y > 0){
+			for(int i = 0;i < x;i++){
+				for(int j = 0;j < y;j++){
+					cout <<'O';
+				}
+				cout << '\n';
+			}
+		}
+		else
+			{
+				cout << "Invalid input";
+			}	
+	return 0;		
+	}
 
 int main(){
 	
